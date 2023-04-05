2fa6866eea7fec1fae515b6137a857a7f6d2d6c0
Tzafqiel <jriem2@uic.edu>
2023-04-05 12:56:47 -0400

    Post
    Post

--- HW3/InstructionRegister_tb.v
+++ HW3/InstructionRegister_tb.v
@@ -1,9 +1,9 @@
 module InstructionRegister_tb();
 reg Data_in[7:0], clk, rst_n;
-wire addr_a[5:0], addr_b[5:0], mux_a[5:0], mux_b[5:0];
+wire Inst;
 integer k,j;
 
-InstructionRegister dut(Data_in, clk, rst_n, addr_a, addr_b, mux_a, mux_b);
+InstructionRegister dut(Data_in, Inst_write, Inst, rst_n, clk);
 
 initial begin
 	clk=0;
@@ -12,6 +12,7 @@ end
 initial begin 
  	rst_n = 1;
  	Data_in = 8'h00;
+	Inst_write = 4'h0;
  	
  	for (k = 0; k < 3; k = k + 1)begin
  		for (j = 0; j < 16; j = j + 1) begin
